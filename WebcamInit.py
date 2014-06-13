069435b22df0e60e32e7c7eabbce25024ff2123a
j <treefort@js-MacBook-Pro.local>
2014-06-12 21:58:52 -0700

    Added image saving capability.

--- WebcamInit.py
+++ WebcamInit.py
@@ -1,5 +1,15 @@
+import os.path
 import cv2
 
+def save_image(i, img):
+    """Checks to see if image file name is free and saves the web cam feed"""
+    fileName = "WebcamFeed_" + str(i) + ".png"
+    if os.path.isfile(fileName):
+        save_image(i + 1, img)
+    else:
+        cv2.imwrite(fileName, img)
+        print 'Saved camera image: ' + "\"" + fileName + "\""
+        
 cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
 
@@ -9,9 +19,14 @@ else:
     rval = False
 
 while rval:
-    cv2.imshow("preview", frame)
+    cv2.imshow("Webcam", frame)
     rval, frame = vc.read()
     key = cv2.waitKey(20)
     if key == 27: # exit on ESC
         break
-cv2.destroyWindow("preview")
+    elif key == ord('s'):
+		save_image(0, frame)
+        
+cv2.destroyWindow("Webcam")
+
+        
