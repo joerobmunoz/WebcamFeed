758360f1abd3fec311d134423fbd8fd99f43edc7
j <treefort@js-MacBook-Pro.local>
2014-06-13 22:58:03 -0700

    Added HaarCascades face detection method.

--- WebcamInit.py
+++ WebcamInit.py
@@ -1,15 +1,37 @@
 import os.path
 import cv2
+import numpy as np
 
+def count_pixels(img):
+	(h, w, d) = img.shape
+	print "Height: {0}, Width: {1}".format(h, w)
+	
+def analyze_face_roi(img):
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+	
+	for (x,y,w,h) in faces:
+    		print "x: {0}, y: {1}".format(x, y)
+    		cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    		save_image(0, img)
+    		#cv2.imshow('img',img)
+    	if not faces:
+    		print "No faces."
+	
 def save_image(i, img):
     """Checks to see if image file name is free and saves the web cam feed"""
+    
     fileName = "WebcamFeed_" + str(i) + ".png"
     if os.path.isfile(fileName):
         save_image(i + 1, img)
     else:
         cv2.imwrite(fileName, img)
+        count_pixels(img)
         print 'Saved camera image: ' + "\"" + fileName + "\""
         
+        
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
 cv2.namedWindow("preview")
 vc = cv2.VideoCapture(0)
 
@@ -25,7 +47,8 @@ while rval:
     if key == 27: # exit on ESC
         break
     elif key == ord('s'):
-		save_image(0, frame)
+		#save_image(0, frame)
+		analyze_face_roi(frame)
         
 cv2.destroyWindow("Webcam")
 
